614db17f00397efcffad195a2954cd7e4a5232be
falsandtru <falsandtru>
2015-02-20 01:13:32 +0900

    Add setup script

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(dirname $0)
+
+ln -sf $SCRIPT_DIR/.profile ~/.profile
+ln -sf $SCRIPT_DIR/.Xmodmap ~/.Xmodmap
+ln -sf $SCRIPT_DIR/.xmonad ~/.xmonad
+ln -sf $SCRIPT_DIR/.vimrc ~/.vimrc
+ln -sf $SCRIPT_DIR/.gitconfig ~/.gitconfig
+
+mkdir -p ~/.vim/bundle
+git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+
