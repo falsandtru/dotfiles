ba5ba499f80c95e29b687587d180ad871f6556fa
falsandtru <falsandtru>
2015-06-13 07:58:38 +0900

    Update setup script

--- develop.sh
+++ develop.sh
@@ -5,6 +5,8 @@ sudo yum install -y git tig
 mkdir -p ~/.vim/bundle
 git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
 
+SCRIPT_DIR=$(dirname $0)
+
 ln -sf $SCRIPT_DIR/.profile ~/.profile
 ln -sf $SCRIPT_DIR/.Xmodmap ~/.Xmodmap
 ln -sf $SCRIPT_DIR/.xmonad ~/.xmonad
